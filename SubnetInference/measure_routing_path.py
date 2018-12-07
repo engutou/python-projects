0075d94ace64ff0c5a6c66355b1345486e5a618b
zhiyong.zhang <zhiyong_zhang1013@163.com>
2018-12-07 15:27:57 +0800

    测量路由路径，设定测量次数

--- SubnetInference/measure_routing_path.py
+++ SubnetInference/measure_routing_path.py
@@ -10,24 +10,29 @@ import traceroute
 import Util
 
 
-def main(datadir):
+def main(datadir, ttl=range(1, 26), repeat=1):
     subnet_list = Util.read_to_list(datadir + 'IpBlocks.txt')
     for net in subnet_list:
         print('Probing:', net)
         net = ipaddress.IPv4Network(net)
-        host_lists = [item for item in net.hosts()]
-        shuffle(host_lists)
+        host_list = list(net.hosts())
 
         count = 0
-        ret_data = ['Vantage:\t' + socket.gethostbyname(socket.gethostname())]
-        for dst in host_lists:
-            rtpath = traceroute.traceroutefast(str(dst), timeout=1, ttl=range(1, 26), ostr=False)
-            rtpath = [item[1] for item in rtpath]
-            ret_data.append(str(dst) + ':\t' + ' '.join(rtpath))
-            count += 1
-            if count % 1e3 == 0:
-                print(count, 'IPs are probed.')
-            Util.write_list(rtpath, 'trace.tmp')
+        ret_data = ['Vantage:\t' + socket.gethostbyname(socket.gethostname()),
+                    'Destination:\t' + str(net),
+                    'TTL:\t' + ' '.join(ttl)]
+
+        while repeat > 0:
+            repeat -= 1
+            shuffle(host_list)
+            for dst in host_list:
+                rtpath = traceroute.traceroutefast(str(dst), timeout=1, ttl=ttl, ostr=False)
+                rtpath = [item[1] for item in rtpath]
+                ret_data.append(str(dst) + ':\t' + ' '.join(rtpath))
+                count += 1
+                if count % 1e3 == 0:
+                    print(count, 'IPs are probed.')
+                Util.write_list(rtpath, 'trace.tmp')
 
         tracefile = datadir + str(net.network_address) + '-' + str(net.prefixlen) + '.trace'
         Util.write_list(ret_data, tracefile)
@@ -35,4 +40,4 @@ def main(datadir):
 
 if __name__ == '__main__':
     datadir = './Dataset/Test/'
-    main(datadir)
+    main(datadir, ttl=range(3, 16), repeat=5)

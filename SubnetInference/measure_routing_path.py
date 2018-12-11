2e83fe8ce81754e6030aee30b1990b7462b57126
zhiyong.zhang <zhiyong_zhang1013@163.com>
2018-12-11 13:55:13 +0800

    traceroute结果中增加‘是否达到目的IP’的信息

--- SubnetInference/measure_routing_path.py
+++ SubnetInference/measure_routing_path.py
@@ -1,5 +1,5 @@
 #! python
-
+# -*- coding:utf-8 -*-
 from random import shuffle
 import ipaddress
 import socket
@@ -29,11 +29,20 @@ def main(datadir, ttl=range(1, 26), repeat=1):
             shuffle(host_list)
             for dst in host_list:
                 rtpath = traceroute.traceroutefast(str(dst), timeout=1, ttl=ttl, ostr=False)
+                # 必须先获取EOP状态
+                dst_reached = (rtpath[-1][-1] == 'EOP')
                 rtpath = [item[1] for item in rtpath]
-                ret_data.append(str(dst) + ':\t' + ' '.join(rtpath))
+                if dst_reached:
+                    # We have reached the destination
+                    ret_data.append('+' + str(dst) + ':\t' + ' '.join(rtpath))
+                else:
+                    # We MAY have not reached the destination
+                    ret_data.append('-' + str(dst) + ':\t' + ' '.join(rtpath))
                 count += 1
                 if count % 1e3 == 0:
                     print(count, 'IPs are probed.')
+
+                rtpath.append('Reached the destination!' if dst_reached else 'MAY not reached the destination!')
                 Util.write_list(rtpath, 'trace.tmp')
 
         tracefile = datadir + str(net.network_address) + '-' + str(net.prefixlen) + '.trace'

1df6ab02c4769268251981f42ba7eae2deaff406
zhiyong.zhang <zhiyong_zhang1013@163.com>
2018-12-12 08:52:14 +0800

    not important: 输出辅助信息

--- SubnetInference/pipeline.py
+++ SubnetInference/pipeline.py
@@ -10,6 +10,7 @@ def predecessor_analysis(ip_graph):
     dst_net = ipaddress.IPv4Network(ip_graph.graph[build_ip_topo_graph.TraceFileHead.dst])
     reached_hosts = set([str(h) for h in dst_net.hosts()])
     reached_hosts = reached_hosts.intersection(set(ip_graph.nodes))
+    print('Number of reached hosts:', len(reached_hosts))
     node2pred = {}
     for node in reached_hosts:
         node2pred[node] = set([pred for pred in ip_graph.predecessors(node)])
@@ -21,7 +22,8 @@ def main():
     if False:
         measure_routing_path.main(datadir)
 
-    trfiles = [datadir + item for item in os.listdir(datadir) if item.endswith('.trace')]
+    # trfiles = [datadir + item for item in os.listdir(datadir) if item.endswith('.trace')]
+    trfiles = [datadir + item for item in ['61.63.92.0-22.trace']]
     for trfile in trfiles:
         # 为每个trace文件处理一次
         print('Processing {0}'.format(trfile))

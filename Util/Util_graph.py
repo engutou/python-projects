4d95e8fc84ba8d7173c27abf179cc638a82940d2
zhiyong-ASUS <zzy5602@gmail.com>
2018-12-17 23:15:13 +0800

    给定一个无向图以及一系列节点（锚节点），移除对连通这些锚节点没有贡献的节点（stub） -- 优化

--- Util/Util_graph.py
+++ Util/Util_graph.py
@@ -17,47 +17,42 @@ def __remove_nontrivial_stubs__(G, anchors):
     :param anchors: 锚节点
     :return nodes_to_remove: 可以被移除的节点
     """
+    # G = nx.Graph()
     nodes_to_remove = []
 
     if len(anchors) <= 1:
-        nodes_to_remove = set(G.nodes).difference(anchors)
         # 除了锚节点，其他节点都可以删除
+        nodes_to_remove = set(G.nodes).difference(anchors)
         return nodes_to_remove
 
     for n in G.nodes:
         if n in anchors or n in nodes_to_remove:
             continue
 
-        anchor2min_cut = {}
         anchors_introduce_multi_node_cut = []
         for a in anchors:
+            # 对于每个节点n，最多寻找两次minimum_node_cut即可判断该节点是否可被删除
             min_cut = nx.minimum_node_cut(G, a, n)
-            anchor2min_cut[a] = min_cut if min_cut else {a}
+            if not min_cut:
+                min_cut = {a}
 
-            if len(anchor2min_cut[a]) >= 2:
+            if len(min_cut) == 1:
+                # 假设移除min_cut，判断节点>n<所在的连通分量中是否存在锚节点
+                edges_to_add_back = list(G.edges(nbunch=min_cut))
+                G.remove_nodes_from(min_cut)
+                for cc in nx.connected_components(G):
+                    if n in cc and (not cc.intersection(anchors)):
+                        # >n<所在的连通分量>cc<不再包含锚节点，可以删除整个连通分量>cc<
+                        nodes_to_remove.extend(cc)
+                        break
+                G.add_edges_from(edges_to_add_back)
+                break
+            else:
                 anchors_introduce_multi_node_cut.append(a)
                 if len(anchors_introduce_multi_node_cut) >= 2:
-                    # 不能删除这个节点
+                    # 不可删除节点>n<
                     break
 
-        if len(anchors_introduce_multi_node_cut) >= 2:
-            # 不能删除这个节点
-            continue
-
-        if len(anchors_introduce_multi_node_cut) == 0:
-            # 所有锚节点到目标节点的min_cut都只包含一个元素
-            min_cuts = {tuple(min_cut) for min_cut in anchor2min_cut.values()}
-            if len(min_cuts) == 1:
-                # todo 可以删除该节点以及该节点与min_cuts[0]中那个元素之间的所有节点
-                nodes_to_remove.append(n)
-
-        else:
-            # 只有一个锚节点>am<到目标节点的min_cut包含多个元素
-            am = anchors_introduce_multi_node_cut[0]
-            cut_list = [cut == {am} for a, cut in anchor2min_cut.items() if a != am]
-            if all(cut_list):
-                # todo 可以删除该节点以及该节点与am之间的所有节点
-                nodes_to_remove.append(n)
     # 返回可移除的节点
     return nodes_to_remove
 
@@ -69,19 +64,19 @@ def remove_stubs(G, anchors):
     :param anchors:
     :return:
     """
+    nodes_to_remove = []
     # 判断是否所有锚节点都在图中
     for i, an in enumerate(anchors):
         if an not in G.nodes:
             print(an, 'is ignored as it\'s not in the graph.')
             del anchors[i]
     if len(anchors) <= 1:
-        print('You must provide at least two anchors.')
-        return False
+        nodes_to_remove = set(G.nodes).difference(anchors)
+        G.remove_nodes_from(nodes_to_remove)
 
     anchors = set(anchors)
 
     # 移除不包含锚节点的连通分量
-    nodes_to_remove = []
     for c in nx.connected_components(G):
         # type(c) = set, a set of nodes
         if not anchors.intersection(c):
@@ -110,12 +105,12 @@ def remove_stubs(G, anchors):
     for c in nx.connected_components(G):
         connected_subgraph = G.subgraph(c)
         sub_anchors = anchors.intersection(connected_subgraph.nodes)
-        _nodes_to_remove = __remove_nontrivial_stubs__(connected_subgraph, sub_anchors)
-        nodes_to_remove.extend(_nodes_to_remove)
-    G.remove_nodes_from(nodes_to_remove)
+        dummy_graph = nx.Graph(connected_subgraph.edges)
 
-    # 全部操作正常执行，返回True
-    return True
+        _nodes_to_remove = __remove_nontrivial_stubs__(dummy_graph, sub_anchors)
+        nodes_to_remove.extend(_nodes_to_remove)
+
+    G.remove_nodes_from(nodes_to_remove)
 
 
 if __name__ == '__main__':
@@ -128,8 +123,8 @@ if __name__ == '__main__':
              (2, 4),
              (2, 5),
              (2, 6),
-             (4, 6),
              (3, 6),
+             (4, 6),
              (5, 15),
              (5, 16),
              (7, 8),
@@ -155,4 +150,4 @@ if __name__ == '__main__':
     G = nx.Graph()
     G.add_edges_from(edges)
     remove_stubs(G, anchors=[0, 1, 2, 3, 20, 1000])
-    print(len(G))
+    print(sorted(list(G.nodes)))
